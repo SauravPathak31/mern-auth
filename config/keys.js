41afdf92cae5107f7126a2dc036590b11bbaa5db
Saurav <pathak_saurav@network.elanco.com>
2020-05-18 16:34:04 +0530

    uri changed

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
+  mongoURI: "mongodb+srv://admin:<password>@myfirstcluster-tnzpi.gcp.mongodb.net/mern_login?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
